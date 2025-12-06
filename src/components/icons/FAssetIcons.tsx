3db023b6e3173e9cf2e5d9237cfc7248bae1100f
Venkateshwar <venkattiwari42@gmail.com>
2025-12-06 09:47:05 +0000

    ## Error Type Build Error
    ## Error Message
    Export Doge doesn't exist in

--- src/components/icons/FAssetIcons.tsx
+++ src/components/icons/FAssetIcons.tsx
@@ -1,8 +1,8 @@
 // Using Lucide-React icons for simplicity, can be replaced with custom SVGs
-import { Bitcoin, Waves, Doge, Litecoin } from 'lucide-react';
+import { Bitcoin, Waves, Dog, Litecoin } from 'lucide-react';
 import type { LucideProps } from 'lucide-react';
 
 export const BtcIcon = (props: LucideProps) => <Bitcoin {...props} />;
 export const XrpIcon = (props: LucideProps) => <Waves {...props} />;
-export const DogeIcon = (props: LucideProps) => <Doge {...props} />;
+export const DogeIcon = (props: LucideProps) => <Dog {...props} />;
 export const LtcIcon = (props: LucideProps) => <Litecoin {...props} />;
