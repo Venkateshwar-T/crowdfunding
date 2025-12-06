9867107b020bea8fb2ef10a59bd837a1de2ddfb2
Venkateshwar <venkattiwari42@gmail.com>
2025-12-06 09:47:42 +0000

    Try fixing this error: `Build Error: Export Litecoin doesn't exist in ta

--- src/components/icons/FAssetIcons.tsx
+++ src/components/icons/FAssetIcons.tsx
@@ -1,8 +1,8 @@
 // Using Lucide-React icons for simplicity, can be replaced with custom SVGs
-import { Bitcoin, Waves, Dog, Litecoin } from 'lucide-react';
+import { Bitcoin, Waves, Dog, CircleDollarSign } from 'lucide-react';
 import type { LucideProps } from 'lucide-react';
 
 export const BtcIcon = (props: LucideProps) => <Bitcoin {...props} />;
 export const XrpIcon = (props: LucideProps) => <Waves {...props} />;
 export const DogeIcon = (props: LucideProps) => <Dog {...props} />;
-export const LtcIcon = (props: LucideProps) => <Litecoin {...props} />;
+export const LtcIcon = (props: LucideProps) => <CircleDollarSign {...props} />;
